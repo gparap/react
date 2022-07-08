ca50dd3b90b74a38d992eebff4106e2a2faa7385
gparap <gparap@protonmail.com>
2022-07-08 21:15:58 +0200

    Initial commit (hello-world)

---
+++ hello-world/src/App.js
@@ -0,0 +1,18 @@
+/* Created by gparap (c) 2022 */
+
+import './App.css'
+
+const header = (
+  <h1>Hello, React!</h1>
+);
+
+function App() {
+  return (
+    <div>
+    {header}
+     <img src="/gparap.png" id="logo"/>
+    </div>
+  );
+}
+
+export default App;
