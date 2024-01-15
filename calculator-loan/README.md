ddf4897273ab74ce4fd25851672a4ad4cb09aa86
gparap <gparap@protonmail.com>
2024-01-15 14:20:02 +0200

    Initial commit (calculator-loan)

---
+++ calculator-loan/README.md
@@ -0,0 +1,3 @@
+# Loan Calculator React App
+
+Responsive Loan Calculator React App that applys the loan payment formula for monthly, total and insterest payments.
---
+++ calculator-loan/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <br />
+        <p>
+          Loan Calculator React App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
