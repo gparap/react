2f6938d543e6b36c76a5eeb887220a6d302aa442
gparap <gparap@protonmail.com>
2024-01-23 14:39:24 +0200

    Update home page
    Make video act as background,
    Style footer,
    Fix copyright

--- website-template/src/routes/Home.js
+++ website-template/src/routes/Home.js
@@ -1,11 +1,10 @@
-/* (c) 2022 gparap */
+/* (c) 2024 gparap */
 import React from 'react'
 import Video from '../assets/videos/video-home.mp4';
 
 function Home() {
   return (
     <div className='container'>
-      <h1>Home Route</h1>
       <div className='container-home'>
 
         {/* Play a video */}
@@ -13,7 +12,22 @@ function Home() {
           playsInline        // Allow video to play inline on iOS devices
           crossOrigin="anonymous"  // Specify CORS handling for the video
         />
-        
+
+        {/* Welcoming content */}
+        <div className="container-home-content">
+          <h1>Welcome to React Website Template</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula
+            odio eget est ullamcorper ultrices. Curabitur vehicula, nisl id suscipit
+            tincidunt, urna libero fermentum nisi, vel commodo ex lectus sed felis.
+            In hac habitasse platea dictumst. Suspendisse potenti. Nunc vehicula
+            sodales vestibulum. Sed vel diam non mauris gravida tincidunt. Nam
+            vulputate risus eu dapibus laoreet. Integer non feugiat nunc. Quisque
+            malesuada ipsum nec risus rhoncus feugiat. Vivamus ultricies, ipsum ut
+            ullamcorper fermentum, elit elit rhoncus lacus, id lacinia metus odio non
+            justo. ...
+          </p>
+        </div>
+
       </div>
     </div>
   )
