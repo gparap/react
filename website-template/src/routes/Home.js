d52b57d886b7934394a2392ac1e49ccac1a7a8cb
gparap <gparap@protonmail.com>
2024-01-23 13:14:41 +0200

    Finish the home page

--- website-template/src/routes/Home.js
+++ website-template/src/routes/Home.js
@@ -1,8 +1,21 @@
+/* (c) 2022 gparap */
 import React from 'react'
+import Video from '../assets/videos/video-home.mp4';
 
 function Home() {
   return (
-    <div>Home Route</div>
+    <div className='container'>
+      <h1>Home Route</h1>
+      <div className='container-home'>
+
+        {/* Play a video */}
+        <video src={Video} loop muted autoPlay
+          playsInline        // Allow video to play inline on iOS devices
+          crossOrigin="anonymous"  // Specify CORS handling for the video
+        />
+        
+      </div>
+    </div>
   )
 }
 
