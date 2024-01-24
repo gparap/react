9e303e9bcd4233e740ef24db4bd2efa0e4e8e5af
gparap <gparap@protonmail.com>
2024-01-24 12:41:06 +0200

    Create contact page with map

--- website-template/src/routes/Contact.js
+++ website-template/src/routes/Contact.js
@@ -1,8 +1,32 @@
+/* (c) 2024 gparap */
 import React from 'react'
+import Map from '../assets/images/map.png'
 
 function Contact() {
   return (
-    <div>Contact Route</div>
+    <div>
+      {/* Contact header */}
+      <h1 className='contact-header'>Contact Us</h1>
+
+      {/* Contact details section */}
+      <div>
+        <h4>Address</h4>
+        <p>123 Main Street, CityVille, around the globe</p>
+      </div>
+      <div>
+        <h4>E-mail</h4>
+        <p>info@template.com</p>
+      </div>
+      <div>
+        <h4>Phone</h4>
+        <p>+99 (123) 456-7890</p>
+      </div>
+
+      {/* Location map */}
+      <div class='container contact-map'>
+        <img src={Map} class='img img-fluid d-sm-none d-md-block w-100' alt='map' />
+      </div>
+    </div>
   )
 }
 
