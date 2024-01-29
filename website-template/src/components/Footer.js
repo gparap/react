e1238d2d916742c9659ba34a31c4e379caeb5c6f
gparap <gparap@protonmail.com>
2024-01-29 12:15:55 +0200

    Update footer & fix layout

--- website-template/src/components/Footer.js
+++ website-template/src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter, FaPinterest } from 'react-icons/fa'
 
 function Footer() {
   return (
     <div className='footer'>
-        <hr />
-        <p>Copyright 2024 gparap</p>
+      <h3>Keep in Touch!</h3>
+      <div className='footer-container-social'>
+        <FaFacebook className='social-icon'/>
+        <FaInstagram className='social-icon'/>
+        <FaTwitter className='social-icon'/>
+        <FaPinterest className='social-icon'/>
+      </div>
+      <hr />
+      <p>Copyright 2024 gparap</p>
     </div>
   )
 }
