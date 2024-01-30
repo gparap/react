9d4558ada0bf38b221935951b5344a562aec0d1d
gparap <gparap@protonmail.com>
2024-01-30 11:37:28 +0200

    Initial commit (image-gallery)

---
+++ image-gallery/README.md
@@ -0,0 +1,3 @@
+# Image Gallery React App
+
+Responsive image gallery with React.
---
+++ image-gallery/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <br />
+        <p>
+          Image Gallery
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
