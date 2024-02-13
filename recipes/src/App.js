6d2cfa156735878b85cd495dea264261a5690b91
gparap <gparap@protonmail.com>
2024-02-13 15:59:02 +0200

    Fetch recipes data using an API

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,8 +1,35 @@
+// © 2024 gparap
 import './App.css';
 import Navigation from './components/Navigation';
 import Search from './components/Search';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  //state for recipes data
+  const [recipes, setRecipes] = useState([]);
+
+  //fetch recipes data from an API using hooks
+  useEffect(() => {
+    const fetchRecipesData = async () => {
+      fetch('<REPLACE-WITH-API>'
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then(recipesData => {
+          setRecipes(recipesData);
+        })
+        .catch((reason) => {
+          console.log(reason);
+        });
+    }
+    fetchRecipesData();
+  }, []);
+
+  //log recipes data to console
+  console.log('Fetched Recipes:', recipes);
+
   return (
     <div className="App">
       <Navigation />
