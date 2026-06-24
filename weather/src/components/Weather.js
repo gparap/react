39942f0002ed4234c17d8f8d91e0b84fdf340488
gparap <54770777+gparap@users.noreply.github.com>
2026-06-24 12:41:07 +0300

    Fetch & Display weather

--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -1,36 +1,73 @@
 /* Copyright (c) 2026 gparap | SPDX-License-Identifier: MIT */
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../App.css'
 import logo from '../logo.png';
 
 const Weather = () => {
+    let api_key = "YOUR_API_KEY_HERE"
+    let api_url = "YOUR_API_URL_HERE"
+
+    const [weatherApiData, setWeatherApiData] = useState(null);
+    const [cityInputData, setCityInputData] = useState("");
+
+    const fetchWeather = async (city) => {
+        try {
+            const response = await fetch(
+                "https://" + api_url + "/city?lang=EN&city=" + encodeURIComponent(city),
+                {
+                    method: "GET",
+                    headers: {
+                        "X-RapidAPI-Key": api_key,
+                        "X-RapidAPI-Host": api_url,
+                        "Content-Type": "application/json",
+                        "Accept": "application/json"
+                    }
+                }
+            );
+            const result = await response.json();
+            setWeatherApiData(result);
+        } catch (error) {
+            console.error("Fetch error:", error);
+        }
+    };
+
+    const handleCityInputData = (e) => {
+        setCityInputData(e.target.value);
+    }
+
+    const searchCityWeather = (city) => {
+        fetchWeather(city);
+    }
+
+    //TODO: convert fahrenheit to celcious using formula: Celsius = (Fahrenheit - 32) / 1.8
+
     return (
         <div className='container'>
             {/* Search Input */}
-            <input className='mt-2' type='text' placeholder='City Name' />
-            <button className='btn btn-primary ms-2' type='button'><i className='bi bi-search'></i></button>
+            <input className='mt-2' type='text' placeholder='City Name' onChange={handleCityInputData} />
+            <button className='btn btn-primary ms-2' type='button' onClick={() => { searchCityWeather(cityInputData) }}><i className='bi bi-search'></i></button>
             {/* Weather Result */}
             <div className=''>Weather</div>
-            <div class='card'>
+            <div className='card'>
                 <img src={logo} className='card-img-top w-25 mx-auto d-block' alt='weather icon' />
-                <div class='card-body'>
-                    <p className='card-text'>TODO:Weather ie Rain</p>
-                    <p className='card-text'>TODO:Description ie light rain</p>
+                <div className='card-body'>
+                    <p className='card-text'>Weather:&nbsp;{weatherApiData?.weather[0]?.main || "Loading ..."}</p>
+                    <p className='card-text'>Description:&nbsp;{weatherApiData?.weather[0]?.description || "Loading..."}</p>
 
                     <div className='d-flex justify-content-center align-items-center'>
-                        <i class="bi bi-thermometer-half"></i>&nbsp;
-                        <p className='card-text'>TODO:temprature ie 21.56&deg;C</p>
+                        <i className="bi bi-thermometer-half"></i>&nbsp;
+                        <p className='card-text'>Temprature:&nbsp;{weatherApiData?.main?.temp || "Loading..."}&nbsp;&deg;F</p>
                     </div>
                     <p />
                     <div className='d-flex justify-content-center align-items-center'>
-                        <i class="bi bi-droplet"></i>&nbsp;
-                        <p className='card-text'>TODO:humidity ie 91%</p>
+                        <i className="bi bi-droplet"></i>&nbsp;
+                        <p className='card-text'>Humidity:&nbsp;{weatherApiData?.main?.humidity || "Loading..."}&nbsp;%</p>
                     </div>
                     <p />
                     <div className='d-flex justify-content-center align-items-center'>
-                        <i class="bi bi-wind"></i>&nbsp;
-                        <p className='card-text'>TODO:wind_speed ie 3.17 meter/sec</p>
+                        <i className="bi bi-wind"></i>&nbsp;
+                        <p className='card-text'>Wind Speed:&nbsp;{weatherApiData?.wind?.speed || "Loading..."}&nbsp;meters/sec</p>
                     </div>
                 </div>
             </div>
